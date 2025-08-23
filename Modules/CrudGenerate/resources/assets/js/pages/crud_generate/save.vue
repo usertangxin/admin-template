04ea8730cbf8482bf36467d98e92817f4d561beb
唐鑫 <1637910092@qq.com>
2025-08-23 19:35:10 +0800

    feat: 添加crud_generate模块的保存页面基础结构

---
+++ Modules/CrudGenerate/resources/assets/js/pages/crud_generate/save.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="m-3 p-3 page-content">
+        <save-form>
+            <a-row>
+                <a-col flex="none">
+                    <a-anchor line-less>
+                        <a-anchor-link href="#table-design">表格设计</a-anchor-link>
+                    </a-anchor>
+                </a-col>
+                <a-col flex="auto">
+                    asdf
+                </a-col>
+            </a-row>
+        </save-form>
+    </div>
+</template>
