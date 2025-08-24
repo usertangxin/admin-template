9acde2a5b1206e12007b3eaf0375b72efbc8adc7
唐鑫 <1637910092@qq.com>
2025-08-24 11:34:56 +0800

    feat(数据库迁移): 添加系统CRUD历史表的额外字段
    添加生成的类名、软删除标志、菜单相关字段到系统CRUD历史表

--- Modules/CrudGenerate/database/migrations/2025_08_23_133737_create_system_crud_histories_table.php
+++ Modules/CrudGenerate/database/migrations/2025_08_23_133737_create_system_crud_histories_table.php
@@ -19,9 +19,15 @@ return new class extends Migration
             $table->string('table_name', 50)->comment('表名');
             $table->json('file_list')->nullable()->comment('文件列表');
             $table->enum('gen_mode', ['app', 'module'])->default('app')->comment('生成方式');
+            $table->string('gen_class_name')->comment('生成的类名');
             $table->string('module_name', 50)->nullable()->comment('模块名');
             $table->json('index_list')->nullable()->comment('数据表索引列表');
             $table->json('column_list')->nullable()->comment('数据表字段列表');
+            $table->boolean('soft_delete')->default(true)->comment('是否软删除');
+            $table->string('parent_menu_code')->nullable()->comment('父菜单编码');
+            $table->string('menu_code')->nullable()->comment('菜单编码');
+            $table->string('menu_name')->nullable()->comment('菜单名称');
+            $table->string('menu_icon')->nullable()->comment('菜单图标');
 
             $table->dateTime('created_at')->nullable()->comment('创建时间');
             $table->dateTime('updated_at')->nullable()->comment('更新时间');
