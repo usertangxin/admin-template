34e525cf33352183f269f0aa78f348e68513e601
唐鑫 <1637910092@qq.com>
2025-09-05 17:19:02 +0800

    feat(GlobalDataPermission): 添加禁用全局数据权限的方法

--- Modules/Admin/app/Traits/GlobalDataPermission.php
+++ Modules/Admin/app/Traits/GlobalDataPermission.php
@@ -4,10 +4,22 @@ namespace Modules\Admin\Traits;
 
 use Modules\Admin\Models\Scopes\GlobalDataPermissionScope;
 
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
 trait GlobalDataPermission
 {
     public static function bootGlobalDataPermission()
     {
         static::addGlobalScope(GlobalDataPermissionScope::class);
+    }
+
+    /**
+     * 禁用全局数据权限
+     * @return mixed 
+     */
+    public static function withoutGlobalDataPermissionScope()
+    {
+        return static::withoutGlobalScope(GlobalDataPermissionScope::class);
     }
 }
