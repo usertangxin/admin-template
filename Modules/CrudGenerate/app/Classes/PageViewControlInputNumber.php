2c83a4435ccf0a0c33271baa9e68b4cc6055da59
唐鑫 <1637910092@qq.com>
2025-09-18 15:26:40 +0800

    feat(PageViewControlInputNumber): 添加输入数字控件的特殊参数支持
    为数字输入控件添加范围、精度、模式和步长等特殊参数支持，通过解析字段配置动态生成属性

--- Modules/CrudGenerate/app/Classes/PageViewControlInputNumber.php
+++ Modules/CrudGenerate/app/Classes/PageViewControlInputNumber.php
@@ -17,9 +17,26 @@ class PageViewControlInputNumber extends AbstractPageViewControl
 
     public function getFormCodeFragment(): string
     {
+        $attrs = '';
+        $range = $this->field['page_view_control_special_params']['range'] ?? [];
+        $precision = $this->field['page_view_control_special_params']['precision'] ?? null;
+        $mode = $this->field['page_view_control_special_params']['mode'] ?? null;
+        $step = $this->field['page_view_control_special_params']['step'] ?? null;
+        if($range) {
+            $attrs .= ":min={$range[0]} :max={$range[1]}";
+        }
+        if($precision) {
+            $attrs .= ":precision={$precision}";
+        }
+        if($mode) {
+            $attrs .= ":mode=\"{$mode}\"";
+        }
+        if($step) {
+            $attrs .= ":step={$step}";
+        }
         return <<<code
             <a-form-item label="{$this->getLabel()}" field="{$this->getFieldName()}">
-                <a-input-number v-model="formData.{$this->getFieldName()}" placeholder="请输入{$this->getComment()}"></a-input-number>
+                <a-input-number v-model="formData.{$this->getFieldName()}" placeholder="请输入{$this->getComment()}" $attrs></a-input-number>
             </a-form-item>
         code;
     }
