75859d168ac120ed4b8b7025fe44474a9463a672
唐鑫 <1637910092@qq.com>
2025-10-04 20:27:03 +0800

    test(admin): 新增视图前缀测试用例

---
+++ Modules/Admin/tests/Unit/ResponseServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Admin\Tests\Unit;
+
+use JetBrains\PhpStorm\NoReturn;
+use Modules\Admin\Services\ResponseService;
+use Tests\TestCase;
+
+class ResponseServiceTest extends TestCase
+{
+    #[NoReturn]
+    public function test_get_view_prefix(): void
+    {
+        $class  = 'Modules\\CrudGenerate\\Http\\Controllers\\TestAbc\\TestController';
+        $result = ResponseService::getViewPrefix($class);
+        $this->assertEquals('module.CrudGenerate.test_abc.test', $result);
+
+        $class  = 'Modules\\CrudGenerate\\Http\\Controllers\\TestAbc';
+        $result = ResponseService::getViewPrefix($class);
+        $this->assertEquals('module.CrudGenerate.test_abc', $result);
+
+        $class  = 'App\\Http\\Controllers\\Auth\\LoginController';
+        $result = ResponseService::getViewPrefix($class);
+        $this->assertEquals('app.auth.login', $result);
+    }
+}
