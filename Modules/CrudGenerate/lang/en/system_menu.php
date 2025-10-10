6b0f22fea4568f1e4df1bac5272b6a4c2c852f25
唐鑫 <1637910092@qq.com>
2025-10-10 15:53:13 +0800

    1.目录多语言

---
+++ Modules/CrudGenerate/lang/en/system_menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'crud_generate_controller' => [
+        'index'                  => 'CRUD Generate',
+        'get_controls'           => 'Get All Controls',
+        'get_field_controls'     => 'Get Field Controls',
+        'get_preview_code'       => 'Preview Code',
+        'post_generate_code'     => 'Generate Code',
+        'get_page_view_controls' => 'Get Page View Controls',
+    ],
+];
