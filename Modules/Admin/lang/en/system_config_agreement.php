212ac69f13722a511b3a0ca953b325a57b8a1886
唐鑫 <1637910092@qq.com>
2025-10-10 21:08:10 +0800

    feat(多语言): 添加系统配置和协议的多语言文件
    添加中文和英文的系统配置分组及协议相关语言文件，用于支持后台管理界面的多语言显示

---
+++ Modules/Admin/lang/en/system_config_agreement.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'user_agreement' => [
+        'value'              => '',
+        'name'               => '用户协议',
+        'config_select_data' => null,
+        'remark'             => '',
+        'input_attr'         => null,
+    ],
+    'privacy_agreement' => [
+        'value'              => '',
+        'name'               => '隐私协议',
+        'config_select_data' => null,
+        'remark'             => '',
+        'input_attr'         => null,
+    ],
+];
