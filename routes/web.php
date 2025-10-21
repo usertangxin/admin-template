481f95e839c756c43af56f69b6a40a46281aafaf
唐鑫 <1637910092@qq.com>
2025-10-21 17:40:50 +0800

    fix(路由): 将根路径重定向到管理员登录页面
    移除测试页面渲染，改为重定向到管理员登录页面以改进用户体验

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('test');
+    // return Inertia::render('test');
+    return redirect()->route('web.admin.login.view');
 });
