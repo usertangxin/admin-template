fd59d9161fbaa3a3a69cc33aa499ebb3ed01eb73
唐鑫 <1637910092@qq.com>
2025-10-23 10:22:16 +0800

    整理格式

--- Modules/Admin/tests/Unit/SystemConfigModelTest.php
+++ Modules/Admin/tests/Unit/SystemConfigModelTest.php
@@ -4,18 +4,17 @@ namespace Modules\Admin\Tests\Unit;
 
 use Modules\Admin\Models\SystemConfig;
 use Modules\Admin\Tests\AbstractAuthTestCase;
-use Tests\TestCase;
 
 class SystemConfigModelTest extends AbstractAuthTestCase
 {
     public function test_value_xss()
     {
         $config = SystemConfig::create([
-            'group' => 'asdf',
-            'name' => 'asdf',
-            'key' => 'asdf',
+            'group'      => 'asdf',
+            'name'       => 'asdf',
+            'key'        => 'asdf',
             'input_type' => 'text',
-            'value' => '<div onclick="alert(\'XSS\');">1</div><script>alert("XSS");</script>',
+            'value'      => '<div onclick="alert(\'XSS\');">1</div><script>alert("XSS");</script>',
         ]);
 
         $this->assertEquals('<div>1</div>', $config->value);
