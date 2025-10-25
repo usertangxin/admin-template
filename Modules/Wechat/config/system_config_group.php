5f8de9a99fedf2d696329f12de2816c7c26a7d17
唐鑫 <1637910092@qq.com>
2025-10-25 10:46:31 +0800

    feat(Wechat): 添加微信模块的系统配置文件和分组配置
    添加了微信模块的系统配置文件(system_config_wechat.php)和分组配置(system_config_group.php)，包含小程序、公众号和微信支付的相关配置项。同时为Pay模块创建了空的配置文件模板，为后续功能扩展做准备。

---
+++ Modules/Wechat/config/system_config_group.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    [
+        'name' => [
+            'zh_CN' => '站点配置',
+            'en'    => 'Site',
+        ],
+        'code'   => 'site_config',
+        'remark' => null,
+    ],
+    [
+        'name' => [
+            'zh_CN' => '上传配置',
+            'en'    => 'Upload',
+        ],
+        'code'   => 'upload_config',
+        'remark' => null,
+    ],
+    [
+        'name' => [
+            'zh_CN' => '邮件服务',
+            'en'    => 'Email Service',
+        ],
+        'code'   => 'email_config',
+        'remark' => null,
+    ],
+    [
+        'name' => [
+            'zh_CN' => '协议',
+            'en'    => 'Agreement',
+        ],
+        'code'   => 'agreement',
+        'remark' => '',
+    ],
+    [
+        'name' => [
+            'zh_CN' => '微信配置',
+            'en'    => 'Wechat',
+        ],
+        'code'   => 'wechat_config',
+        'remark' => [
+            'zh_CN' => '公众号，小程序，微信支付等',
+            'en'    => 'Official Account, Mini Program, Wechat Payment, etc.',
+        ],
+    ],
+];
