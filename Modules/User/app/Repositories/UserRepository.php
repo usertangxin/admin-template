32bea1f5e280023f66d244a754b712762a82e04d
唐鑫 <1637910092@qq.com>
2025-11-01 17:18:16 +0800

    feat(user): 实现用户账户资金与积分操作功能
    - 新增余额充值、提现、消费、退款、冻结与解冻操作
    - 新增佣金发放、提现、冻结与解冻操作- 新增积分增加、抵扣、冻结与解冻操作- 所有操作均通过数据库事务确保数据一致性
    - 添加金额与积分正数校验，防止非法操作
    - 每个操作均记录详细日志，便于审计与追踪

--- Modules/User/app/Repositories/UserRepository.php
+++ Modules/User/app/Repositories/UserRepository.php
@@ -2,8 +2,14 @@
 
 namespace Modules\User\Repositories;
 
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Modules\Admin\Repositories\AbstractRepository;
 use Modules\User\Models\User;
+use Modules\User\Models\UserBalanceLog;
+use Modules\User\Models\UserCommissionLog;
+use Modules\User\Models\UserIntegralLog;
+use Throwable;
 
 class UserRepository extends AbstractRepository
 {
@@ -14,33 +20,393 @@ class UserRepository extends AbstractRepository
 
     /**
      * 充值余额
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
      */
     public function rechargeBalance($user_id, $balance, $memo)
     {
-        // TODO: Implement rechargeBalance() method.
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('充值金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => $balance, // 正数表示增加
+                'memo'      => $memo,
+                'operation' => 'recharge',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
     }
 
     /**
-     * 扣除余额
+     * 提现
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
      */
-    public function deductionBalance($user_id, $balance, $memo)
+    public function withdrawBalance($user_id, $balance, $memo)
     {
-        // TODO: Implement deductionBalance() method.
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('提现金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => -$balance, // 负数表示减少
+                'memo'      => $memo,
+                'operation' => 'withdraw',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 消费
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
+     */
+    public function consumptionBalance($user_id, $balance, $memo)
+    {
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('消费金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => -$balance, // 负数表示减少
+                'memo'      => $memo,
+                'operation' => 'consumption',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 退款
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
+     */
+    public function refundBalance($user_id, $balance, $memo)
+    {
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('退款金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => $balance, // 正数表示增加
+                'memo'      => $memo,
+                'operation' => 'refund',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
     }
 
     /**
      * 冻结余额
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
      */
     public function freezeBalance($user_id, $balance, $memo)
     {
-        // TODO: Implement freezeBalance() method.
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('冻结金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => -$balance, // 负数表示从可用余额中减少
+                'memo'      => $memo,
+                'operation' => 'freeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
     }
 
     /**
      * 解冻余额
+     *
+     * @return mixed|UserBalanceLog
+     *
+     * @throws Throwable
      */
     public function unFreezeBalance($user_id, $balance, $memo)
     {
-        // TODO: Implement unFreezeBalance() method.
+        // 确保金额为正数
+        if ($balance < 0) {
+            throw new Exception('解冻金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $balance, $memo) {
+            $log = new UserBalanceLog([
+                'user_id'   => $user_id,
+                'balance'   => $balance, // 正数表示增加到可用余额
+                'memo'      => $memo,
+                'operation' => 'unfreeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 消费返佣（发放佣金）
+     *
+     * @return mixed|UserCommissionLog
+     *
+     * @throws Throwable
+     */
+    public function consumptionReturnsCommission($user_id, $commission, $memo)
+    {
+        // 确保金额为正数
+        if ($commission < 0) {
+            throw new Exception('发放佣金金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $commission, $memo) {
+            $log = new UserCommissionLog([
+                'user_id'    => $user_id,
+                'commission' => $commission, // 正数表示增加
+                'memo'       => $memo,
+                'operation'  => 'consumption_returns_commission',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 提现佣金
+     *
+     * @return mixed|UserCommissionLog
+     *
+     * @throws Throwable
+     */
+    public function withdrawCommission($user_id, $commission, $memo)
+    {
+        // 确保金额为正数
+        if ($commission < 0) {
+            throw new Exception('提现佣金金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $commission, $memo) {
+            $log = new UserCommissionLog([
+                'user_id'    => $user_id,
+                'commission' => -$commission, // 负数表示减少
+                'memo'       => $memo,
+                'operation'  => 'withdraw',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 冻结佣金
+     *
+     * @return mixed|UserCommissionLog
+     *
+     * @throws Throwable
+     */
+    public function freezeCommission($user_id, $commission, $memo)
+    {
+        // 确保金额为正数
+        if ($commission < 0) {
+            throw new Exception('冻结佣金金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $commission, $memo) {
+            $log = new UserCommissionLog([
+                'user_id'    => $user_id,
+                'commission' => -$commission, // 负数表示从可用佣金中减少
+                'memo'       => $memo,
+                'operation'  => 'freeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 解冻佣金
+     *
+     * @return mixed|UserCommissionLog
+     *
+     * @throws Throwable
+     */
+    public function unFreezeCommission($user_id, $commission, $memo)
+    {
+        // 确保金额为正数
+        if ($commission < 0) {
+            throw new Exception('解冻佣金金额不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $commission, $memo) {
+            $log = new UserCommissionLog([
+                'user_id'    => $user_id,
+                'commission' => $commission, // 正数表示增加到可用佣金
+                'memo'       => $memo,
+                'operation'  => 'unfreeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 消费返积分（增加积分）
+     *
+     * @return mixed|UserIntegralLog
+     *
+     * @throws Throwable
+     */
+    public function consumptionReturnsIntegral($user_id, $integral, $memo)
+    {
+        // 确保积分为正数
+        if ($integral < 0) {
+            throw new Exception('增加积分数量不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $integral, $memo) {
+            $log = new UserIntegralLog([
+                'user_id'   => $user_id,
+                'integral'  => $integral, // 正数表示增加
+                'memo'      => $memo,
+                'operation' => 'consumption_returns_integral',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 抵扣积分
+     *
+     * @return mixed|UserIntegralLog
+     *
+     * @throws Throwable
+     */
+    public function deductionIntegral($user_id, $integral, $memo)
+    {
+        // 确保积分为正数
+        if ($integral < 0) {
+            throw new Exception('抵扣积分数量不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $integral, $memo) {
+            $log = new UserIntegralLog([
+                'user_id'   => $user_id,
+                'integral'  => -$integral, // 负数表示减少
+                'memo'      => $memo,
+                'operation' => 'deduction',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 冻结积分
+     *
+     * @return mixed|UserIntegralLog
+     *
+     * @throws Throwable
+     */
+    public function freezeIntegral($user_id, $integral, $memo)
+    {
+        // 确保积分为正数
+        if ($integral < 0) {
+            throw new Exception('冻结积分数量不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $integral, $memo) {
+            $log = new UserIntegralLog([
+                'user_id'   => $user_id,
+                'integral'  => -$integral, // 负数表示从可用积分中减少
+                'memo'      => $memo,
+                'operation' => 'freeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
+    }
+
+    /**
+     * 解冻积分
+     *
+     * @return mixed|UserIntegralLog
+     *
+     * @throws Throwable
+     */
+    public function unFreezeIntegral($user_id, $integral, $memo)
+    {
+        // 确保积分为正数
+        if ($integral < 0) {
+            throw new Exception('解冻积分数量不能为负数');
+        }
+
+        return DB::transaction(function () use ($user_id, $integral, $memo) {
+            $log = new UserIntegralLog([
+                'user_id'   => $user_id,
+                'integral'  => $integral, // 正数表示增加到可用积分
+                'memo'      => $memo,
+                'operation' => 'unfreeze',
+            ]);
+
+            $log->save();
+
+            return $log;
+        });
     }
 }
