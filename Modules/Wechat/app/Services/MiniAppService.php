1b513493edeb97c6b5c7fad1188ebdea7d6d9967
唐鑫 <1637910092@qq.com>
2025-11-02 10:51:52 +0800

    feat(wechat): 更新微信小程序配置项
    - 在微信小程序配置中添加新的 token 和 aes key
    - 增强多语言支持，提供中文和英文备注信息
    - 统一配置项格式，提升可读性和易用性

---
+++ Modules/Wechat/app/Services/MiniAppService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Wechat\Services;
+
+use EasyWeChat\MiniApp\Application;
+use Modules\Admin\Services\SystemConfigService;
+
+class MiniAppService
+{
+    protected Application $app;
+
+    public function __construct()
+    {
+        $app_id = app(SystemConfigService::class)->getConfigByKey('wechat_mini_app_id');
+        $secret = app(SystemConfigService::class)->getConfigByKey('wechat_mini_secret');
+        $token = app(SystemConfigService::class)->getConfigByKey('wechat_mini_token');
+        $aes_key = app(SystemConfigService::class)->getConfigByKey('wechat_mini_aes_key');
+
+        $config = [
+            'app_id' => $app_id,
+            'secret' => $secret,
+            'token' => $token,
+            'aes_key' => $aes_key,
+        ];
+
+        $this->app = new Application($config);
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return $this->app->{$name}(...$arguments);
+    }
+}
---
+++ Modules/Wechat/app/Services/OfficialAccountService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Wechat\Services;
+
+use Modules\Admin\Services\SystemConfigService;
+use EasyWeChat\OfficialAccount\Application;
+
+/**
+ * @mixin Application
+ * @package Modules\Wechat\Services
+ */
+class OfficialAccountService
+{
+    protected Application $app;
+
+    public function __construct()
+    {
+        $app_id = app(SystemConfigService::class)->getConfigByKey('wechat_official_app_id');
+        $secret = app(SystemConfigService::class)->getConfigByKey('wechat_official_secret');
+        $token = app(SystemConfigService::class)->getConfigByKey('wechat_official_token');
+        $aes_key = app(SystemConfigService::class)->getConfigByKey('wechat_official_aes_key');
+        $oauth_scopes = app(SystemConfigService::class)->getConfigByKey('wechat_official_oauth_scopes');
+        $redirect_url = app(SystemConfigService::class)->getConfigByKey('wechat_official_redirect_url');
+
+        $config = [
+            'app_id' => $app_id,
+            'secret' => $secret,
+            'token' => $token,
+            'aes_key' => $aes_key,
+            'oauth' => [
+                'scopes' => $oauth_scopes,
+                'callback' => $redirect_url,
+            ],
+        ];
+
+        $this->app = new Application($config);
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return $this->app->{$name}(...$arguments);
+    }
+}
